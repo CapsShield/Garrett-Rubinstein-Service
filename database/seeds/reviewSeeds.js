f97a2806ed1dda0b7580dce00fa9d879478c586e
Garrett Rubinstein <garrub.bergen@gmail.com>
2021-01-15 00:19:05 -0600

    Added seed script and fixed issue with user/game ids in review seeds

--- database/seeds/reviewSeeds.js
+++ database/seeds/reviewSeeds.js
@@ -8,14 +8,14 @@ for (var i = 0; i < usersGames.length; i++) {
       min: 0,
       max: 100
     }) < 90 ? 1 : 2,
-    gameId: usersGames.gameId,
-    userId: usersGames.userId,
+    gameId: usersGames[i].gameId,
+    userId: usersGames[i].userId,
     helpfulVotes: faker.random.number(1000),
     unhelpfulVotes: faker.random.number(1000),
     funnyVotes: faker.random.number(1000),
     hoursWhenReviewed: faker.random.number({
       min: 0,
-      max: usersGames.hoursPlayed,
+      max: usersGames[i].hoursPlayed,
       precision: 0.1
     }),
     createdAt: faker.date.past(),
