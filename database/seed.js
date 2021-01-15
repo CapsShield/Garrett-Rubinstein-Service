9e8c42d8cddd07ed66f752b1316a1ad98bfe08a4
Garrett Rubinstein <garrub.bergen@gmail.com>
2021-01-14 19:38:35 -0600

    refactored seeding to allow for testing

--- database/seed.js
+++ database/seed.js
@@ -1,58 +1,21 @@
 const db = require('./index.js');
-const {User, Game, Language, Review, UsersGames} = require('./models/index.js');
+const { User, Game, Language, Review, UsersGames } = require('./models/index.js');
 
-const userSeeds = [
-  {
-    username: 'Garrub',
-    avatarUrl: 'http://placecorgi.com/32/32',
-    productsOwned: 42,
-    reviewsWritten: 1
-  }
-];
-const gameSeeds = [
-  {
-    title: 'Antichamber'
-  }
-];
-const langSeeds = [
-  {
-    lang: 'en'
-  }
-];
-const reviewSeeds = [
-  {
-    reviewText: 'OMG what a cool puzzle game! It really messes with your brain! 10/10, would reccommend!',
-    helpfulVotes: 200,
-    unhelpfulVotes: 15,
-    funnyVotes: 3,
-    hoursWhenReviewed: 24.5,
-    createdAt: new Date(2013, 3, 15, 9, 38, 16),
-    languageId: 1,
-    gameId: 1,
-    userId: 1
-  }
-];
-const usersGamesSeeds = [
-  {
-    hoursPlayed: 44,
-    purchasedOnSteam: true,
-    gameId: 1,
-    userId: 1
-  }
-];
-
-const seed = () => {
-  return User.bulkCreate(userSeeds)
-    .then(() => Game.bulkCreate(gameSeeds))
-    .then(() => Language.bulkCreate(langSeeds))
-    .then(() => Review.bulkCreate(reviewSeeds))
-    .then(() => UsersGames.bulkCreate(usersGamesSeeds))
+const seed = (seeds) => {
+  return User.bulkCreate(seeds.user)
+    .then(() => Game.bulkCreate(seeds.game))
+    .then(() => Language.bulkCreate(seeds.lang))
+    .then(() => Review.bulkCreate(seeds.review))
+    .then(() => UsersGames.bulkCreate(seeds.usersGames))
     .catch((err) => console.log(`ERROR IN SEEDING: \n ${err}`));
 };
 
-db.sync({force: true})
-  .then(() => seed())
-  .then(() => {
-    process.exit();
-  })
-  .catch((err) => console.log(`ERROR IN SYNC: \n ${err}`));
+module.exports = (seeds, cb) => {
+  return db.sync({ force: true })
+    .then(() => seed(seeds))
+    .then(() => cb(null))
+    .then(() => {
+      process.exit();
+    })
+    .catch((err) => cb(err));
+};
---
+++ tests/testSeed.js
@@ -0,0 +1,52 @@
+const seedAll = require('../database/seed.js');
+const db = require('../database/index.js');
+
+const seeds = {
+  user: [
+    {
+      username: 'Garrub',
+      avatarUrl: 'http://placecorgi.com/32/32',
+      productsOwned: 42,
+      reviewsWritten: 1
+    }
+  ],
+  game: [
+    {
+      title: 'Antichamber'
+    }
+  ],
+  lang: [
+    {
+      lang: 'en'
+    }
+  ],
+  review: [
+    {
+      reviewText: 'OMG what a cool puzzle game! It really messes with your brain! 10/10, would reccommend!',
+      helpfulVotes: 200,
+      unhelpfulVotes: 15,
+      funnyVotes: 3,
+      hoursWhenReviewed: 24.5,
+      createdAt: new Date(2013, 3, 15, 9, 38, 16),
+      languageId: 1,
+      gameId: 1,
+      userId: 1
+    }
+  ],
+  usersGames: [
+    {
+      hoursPlayed: 44,
+      purchasedOnSteam: true,
+      gameId: 1,
+      userId: 1
+    }
+  ]
+};
+
+seedAll(seeds, (err) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log('SUCCESS!');
+  }
+});
