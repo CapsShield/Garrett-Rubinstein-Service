27ebc9fbc3c8d218611f3f3a2b0f403957773f56
Garrett Rubinstein <garrub.bergen@gmail.com>
2021-01-15 19:03:59 -0600

    added test for query w/pagination (dont return more than 10 results)

--- tests/index.js
+++ tests/index.js
@@ -1,4 +1,9 @@
 const testSeed = require('./testSeed.js');
+const testQuery = require('./testQuery.js');
+
+//testSeed(() => console.log('\n*** SEED TESTS ***\n'));
+var gamesToTest = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+gamesToTest.forEach((gameId) => testQuery(() => console.log(`\n*** QUERY TESTS ${gameId} ***\n`), gameId));
 
 
-testSeed(() => console.log('\n*** SEED TESTS ***\n'));
+
