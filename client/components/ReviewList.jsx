8cdf3db7edea0be887cb71a654eecf706b5afc8b
Garrett Rubinstein <garrub.bergen@gmail.com>
2021-01-16 23:20:52 -0600

    remove extraneous console log

--- client/components/ReviewList.jsx
+++ client/components/ReviewList.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReviewEntry from './ReviewEntry.jsx';
 
 const ReviewList = (props) => {
-  console.log(props);
   return props.reviews.map((review) => (
     <ReviewEntry review={review} key={review.id}/>
   ));
