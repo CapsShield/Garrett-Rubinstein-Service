d5d3ab5389e08a4cf85ea90c7fa99321cfc06392
Garrett Rubinstein <garrub.bergen@gmail.com>
2021-01-19 15:24:39 -0600

    summary count now uses dynamic data

--- client/components/SummaryBar.jsx
+++ client/components/SummaryBar.jsx
@@ -7,7 +7,7 @@ const SummaryBar = (props) => {
         <div className="summary-title">Overall Reviews:</div>
         <div className="summary-details">
           <div className="summary-score">Overwhelmingly Positive</div>
-          <div className="summary-review-count">(10,820 reviews)</div>
+          <div className="summary-review-count">{`(${props.overallSummary[1]} review${props.overallSummary !== 1 ? 's' : null})`}</div>
           <div className="tooltip summary-tooltip"></div>
         </div>
       </div>
@@ -15,7 +15,7 @@ const SummaryBar = (props) => {
         <div className="summary-title">Recent Reviews:</div>
         <div className="summary-details">
           <div className="summary-score">Overwhelmingly Positive</div>
-          <div className="summary-review-count">(144 reviews)</div>
+          <div className="summary-review-count">{`(${props.recentSummary[1]} review${props.recentSummary !== 1 ? 's' : null})`}</div>
           <div className="tooltip summary-tooltip"></div>
         </div>
       </div>
