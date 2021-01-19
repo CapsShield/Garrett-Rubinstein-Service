6d2a6fd474ccccce6cdf4bf40d7422b18cf88014
Garrett Rubinstein <garrub.bergen@gmail.com>
2021-01-19 14:08:05 -0600

    request summary data upon App mount

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -5,11 +5,21 @@ import SummaryBar from './SummaryBar.jsx';
 
 const App = (props) => {
   var [reviews, setReviews] = useState([]);
+  var [overallSummary, setOverallSummary] = useState([0, 1]);
+  var [recentSummary, setRecentSummary] = useState([0, 1]);
   useEffect(() => {
     //fetch page 0 of reviews
     fetch(`/api/games/${props.gameId || 1}/reviews/0`)
       .then(response => response.json())
       .then(parsed => setReviews(parsed.rows))
+      .catch(err => console.error(err));
+
+    fetch(`/api/games/${props.gameId || 1}/summary`)
+      .then(response => response.json())
+      .then(parsed => {
+        setOverallSummary(parsed.overall);
+        setRecentSummary(parsed.recent);
+      })
       .catch(err => console.error(err));
   }, []);
 
