a0abb0b8e12476f87b7044b085a62adfaf660ddb
Garrett Rubinstein <garrub.bergen@gmail.com>
2021-01-20 13:09:46 -0600

    display text when no reviews exist

--- client/components/ReviewList.jsx
+++ client/components/ReviewList.jsx
@@ -2,6 +2,13 @@ import React from 'react';
 import ReviewEntry from './ReviewEntry.jsx';
 
 const ReviewList = (props) => {
+  if (props.reviews.length === 0) {
+    return <div className="no-reviews">
+      <div>There are no more reviews that match the filters set above</div>
+      <div>Adjust the filters above to see other reviews</div>
+    </div>;
+  }
+
   return props.reviews.map((review) => (
     <ReviewEntry review={review} key={review.id}/>
   ));
