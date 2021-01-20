00339e143fae3560d29a5ba4dfe9a8e7f79d122f
Garrett Rubinstein <garrub.bergen@gmail.com>
2021-01-20 14:41:55 -0600

    created enzyme tests for ReviewList, tests are passing

--- client/components/tests/ReviewList.test.jsx
+++ client/components/tests/ReviewList.test.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
+import {shallow} from '../../enzyme';
 import ReviewList from '../ReviewList';
+import ReviewEntry from '../ReviewEntry';
 
-test('this test works', () => {
-  const component = renderer.create(
-    <ReviewList reviews={[]} />
-  );
-  expect(component).toBeDefined();
+
+describe('ReviewList tests', () => {
+  it('should only render a no-reviews div when review array is empty', () => {
+    const reviews = [];
+    const wrapper = shallow(<ReviewList reviews={reviews}/>);
+    expect(wrapper.find('.no-reviews')).toHaveLength(1);
+    expect(wrapper.find(ReviewEntry)).toHaveLength(0);
+  });
+  it('should only render ReviewEntries equal to the length of the passed in review array', () => {
+    const reviews = [{id: 0}, {id: 1}, {id: 2}, {id: 3}, {id: 4}];
+    const wrapper = shallow(<ReviewList reviews={reviews}/>);
+    expect(wrapper.find(ReviewEntry)).toHaveLength(5);
+    expect(wrapper.find('.no-reviews')).toHaveLength(0);
+  });
 });
