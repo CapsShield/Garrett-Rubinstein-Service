5063d6b029a74282538514fb8fe27ead32732e8d
Garrett Rubinstein <garrub.bergen@gmail.com>
2021-01-21 19:31:10 -0600

    migrated first third or so of review entry styles to styled components

--- client/components/ReviewEntry.jsx
+++ client/components/ReviewEntry.jsx
@@ -1,23 +1,23 @@
 import React from 'react';
+import styled from 'styled-components';
 const moment = require('moment');
 
 const ReviewEntry = ({ review }) => {
   return (
-    <div className="review-entry">
-      <div className="gradient-top-border">
-      </div>
-      <div className="review-user-info">
-        <div className="user-info-ctn">
-          <div className="avatar">
-            <img className="avatar-img" src={review.user.avatarUrl} />
-          </div>
-          <div className="username">{review.user.username}</div>
-          <div className="num-owned-games">{`${review.user.productsOwned} product${review.user.productsOwned !== 1 ? 's' : null} in account`}</div>
-          <div className="num-reviews">{`${review.user.reviewsWritten} review${review.user.reviewsWritten !== 1 ? 's' : null}`}</div>
-        </div>
-      </div>
-      <div className="review-info">
-        <div className="review-header">
+    <ReviewEntryContainer className="review-entry">
+      <GradientTopBorder className="gradient-top-border" />
+      <ReviewUserInfo className="review-user-info">
+        <UserInfoContainer className="user-info-ctn">
+          <Avatar className="avatar">
+            <AvatarImg className="avatar-img" src={review.user.avatarUrl} />
+          </Avatar>
+          <Username className="username">{review.user.username}</Username>
+          <NumOwnedGames className="num-owned-games">{`${review.user.productsOwned} product${review.user.productsOwned !== 1 ? 's' : null} in account`}</NumOwnedGames>
+          <NumReviews className="num-reviews">{`${review.user.reviewsWritten} review${review.user.reviewsWritten !== 1 ? 's' : null}`}</NumReviews>
+        </UserInfoContainer>
+      </ReviewUserInfo>
+      <ReviewInfo className="review-info">
+        <ReviewHeader className="review-header">
           <div className="rec-thumb">
             <img src={`assets/${review.positive ? 'thumbUp' : 'thumbDown'}.png`} />
           </div>
@@ -31,7 +31,7 @@ const ReviewEntry = ({ review }) => {
           <div className="review-source">
             <img src={`assets/${review.user.games[0].usersgame.purchasedOnSteam ? 'icon_review_steam' : 'icon_review_key'}.png`} />
           </div>
-        </div>
+        </ReviewHeader>
         <div className="posted-date">{`posted: ${moment(new Date(review.createdAt)).format('MMMM D, YYYY')}`}</div>
         <div className="review-content">
           {review.reviewText}
@@ -62,9 +62,104 @@ const ReviewEntry = ({ review }) => {
           <div className="helpful-count">{`${review.helpfulVotes} ${review.helpfulVotes === 1 ? 'person' : 'people'} found this review helpful`}</div>
           <div className="funny-count">{`${review.funnyVotes} ${review.funnyVotes === 1 ? 'person' : 'people'} found this review funny`}</div>
         </div>
-      </div>
-    </div>
+      </ReviewInfo>
+    </ReviewEntryContainer>
   );
 };
 
+const ReviewEntryContainer = styled.div`
+  background-color: rgba(0, 0, 0, 0.2);
+  display: grid;
+  width: 616px;
+  grid-template-columns: 200px 1fr;
+  grid-template-rows: 1px 1fr;
+  margin-bottom: 24px;
+`;
+const GradientTopBorder = styled.div`
+  height: 1px;
+  grid-row: 1;
+  grid-column: 1 / span 2;
+  background-image: url("assets/maincol_gradient_rule.png");
+`;
+const ReviewUserInfo = styled.div`
+  grid-column: 1;
+  grid-row: 2;
+  display: flex;
+  flex-direction: column;
+  padding: 8px;
+  width: 200px;
+`;
+const UserInfoContainer = styled.div`
+  display: grid;
+  grid-template-columns: 44px 1fr;
+  grid-template-rows: 19px 17px 12px;
+  color: #7b8ea3;
+  ${ReviewEntryContainer}:hover & {
+    color: #C1DBF4;
+  }
+`;
+const Avatar = styled.div`
+  grid-row: 1 / span 2;
+  grid-column: 1;
+  padding: 2px;
+  height: 36px;
+  width: 36px;
+  background-image: linear-gradient(rgb(83, 164, 196) 5%, rgb(69, 128, 151) 95%);
+  background-clip: border-box;
+`;
+const AvatarImg = styled.img`
+  height: 32px;
+  width: 32px;
+`;
+const Username = styled.div`
+  grid-row: 1;
+  grid-column: 2;
+  font-size: 13px;
+  font-weight: bold;
+  max-width: 140px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  &:hover {
+    cursor: pointer;
+  }
+`;
+const NumOwnedGames = styled.div`
+  grid-row: 2;
+  grid-column: 2;
+  font-size: 11px;
+  width: fit-content;
+  &:hover {
+    cursor: pointer;
+    color: #67c1f5;
+  }
+`;
+const NumReviews = styled.div`
+  grid-row: 3;
+  grid-column: 1 / span 2;
+  font-size: 11px;
+  width: fit-content;
+  &:hover {
+    cursor: pointer;
+    color: #67c1f5;
+  }
+`;
+const ReviewInfo = styled.div`
+  grid-column: 2;
+  grid-row: 2;
+  display: flex;
+  flex-direction: column;
+  margin-left: 14px;
+  justify-content: center;
+`;
+const ReviewHeader = styled.div`
+  display: flex;
+  background-color: rgba(0, 0, 0, 0.2);
+  margin-top: 8px;
+  margin-bottom: 13px;
+  height: 40px;
+  &:hover {
+    background-color: rgba( 255, 255, 255, 0.1 );
+    cursor: pointer;
+  }
+`;
 export default ReviewEntry;
