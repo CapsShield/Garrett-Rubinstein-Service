ad3a5820f8c903cd0d106552aa5dc7375e4a6da2
Garrett Rubinstein <garrub.bergen@gmail.com>
2021-01-21 18:34:40 -0600

    centered App, added state for pagination

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,18 +1,25 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
 import ReviewList from './Reviewlist.jsx';
 import SummaryBar from './SummaryBar.jsx';
 import FilterBar from './FilterBar.jsx';
+
 
 
 const App = (props) => {
   var [reviews, setReviews] = useState([]);
   var [overallSummary, setOverallSummary] = useState([0, 1]);
   var [recentSummary, setRecentSummary] = useState([0, 1]);
+  var [page, setPage] = useState(1);
+  var [total, setTotal] = useState(1);
   useEffect(() => {
     //fetch page 0 of reviews
     fetch(`/api/games/${props.gameId || 1}/reviews/0`)
       .then(response => response.json())
-      .then(parsed => setReviews(parsed.rows))
+      .then(parsed => {
+        setReviews(parsed.rows);
+        setTotal(parsed.count);
+      })
       .catch(err => console.error(err));
 
     fetch(`/api/games/${props.gameId || 1}/summary`)
@@ -24,13 +31,34 @@ const App = (props) => {
       .catch(err => console.error(err));
   }, []);
 
+  const changePage = (newPage) => {
+    fetch(`/api/games/${props.gameId || 1}/reviews/${newPage - 1}`)
+      .then(response => response.json())
+      .then(parsed => {
+        setReviews(parsed.rows);
+        setTotal(parsed.count);
+        setPage(newPage);
+      })
+      .catch(err => console.error(err));
+  };
+
   return (
-    <div>
-      <SummaryBar overallSummary={overallSummary} recentSummary={recentSummary}/>
-      <FilterBar positive={overallSummary[0]} allReviews={overallSummary[1]}/>
-      <ReviewList reviews={reviews}/>
-    </div>
+    <GridContainer>
+      <AppContainer>
+        <SummaryBar overallSummary={overallSummary} recentSummary={recentSummary} />
+        <FilterBar positive={overallSummary[0]} allReviews={overallSummary[1]} />
+        <ReviewList reviews={reviews} page={page} changePage={changePage} total={total}/>
+      </AppContainer>
+    </GridContainer>
   );
 };
+
+const GridContainer = styled.div`
+  display: grid;
+  grid-column-template: 1fr 940px 1fr
+`;
+const AppContainer = styled.div`
+  grid-column: 2;
+`;
 
 export default App;
