b9b8941f5f6bb9c1b1ac824f2fc82765c26880fc
Garrett Rubinstein <garrub.bergen@gmail.com>
2021-01-22 18:24:28 -0600

    minor: removed comment crud

--- database/models/usersgames.model.js
+++ database/models/usersgames.model.js
@@ -4,20 +4,6 @@ const Game = require('./games.model.js');
 const User = require('./users.model.js');
 
 const UsersGames = db.define('usersgame', {
-  /*GameId: {
-    type: Sequelize.INTEGER,
-    references: {
-      model: Game,
-      key: 'id'
-    }
-  },
-  UserId: {
-    type: Sequelize.INTEGER,
-    references: {
-      model: User,
-      key: 'id'
-    }
-  }*/
 }, {
   timestamps: false
 });
