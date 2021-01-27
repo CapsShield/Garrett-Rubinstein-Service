8514f37400952530ef7dad3a31870f7c62d45044
Garrett Rubinstein <garrub.bergen@gmail.com>
2021-01-27 17:47:51 -0600

    abstracted mysql credentials to env vars

--- database/index.js
+++ database/index.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-module.exports = new Sequelize('FEC', 'student', 'student', {
+module.exports = new Sequelize('FEC', process.env.MYSQL_USER, process.env.MYSQL_PWD, {
   host: 'localhost',
   dialect: 'mysql',
   logging: false
